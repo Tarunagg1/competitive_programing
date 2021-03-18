8cf5800f99018255ae7d2e8ab68e3f97668c7f22
tarun <arunaggarwal096@gmail.com>
2021-03-18 17:53:44 +0530

    day 17

---
+++ geeksforgeeks/Majority Element.cpp
@@ -0,0 +1,69 @@
+/*
+Given an array A of N elements. Find the majority element in the array. A majority element in an array A of size N is an element that appears more than N/2 times in the array.
+ 
+
+Example 1:
+
+Input:
+N = 3 
+A[] = {1,2,3} 
+Output:
+-1
+Explanation:
+Since, each element in 
+{1,2,3} appears only once so there 
+is no majority element.
+Example 2:
+
+Input:
+N = 5 
+A[] = {3,1,3,3,2} 
+Output:
+3
+Explanation:
+Since, 3 is present more
+than N/2 times, so it is 
+the majority element.
+
+Your Task:
+The task is to complete the function majorityElement() which returns the majority element in the array. If no majority exists, return -1.
+ 
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(1).
+ 
+
+Constraints:
+1 <= N <= 107
+0 <= Ai <= 106
+
+*/
+
+int majorityElement(int arr[], int n)
+    {
+        
+        int count =1;int res = 0;
+        for(int i=0; i<n; i++){
+            if(arr[res] == arr[i]){
+                count++;
+            }else{
+                count--;
+            }
+            if(count == 0){
+                count = 1;
+                res = i;
+            }
+        }
+        
+        count = 0;
+        for(int i=0; i<n; i++){
+            if(arr[res] == arr[i]){
+                count++;
+            }
+        }
+        if(count <= (n/2)){
+            return -1;
+        }
+        else
+            return arr[res];
+    }
---
+++ geeksforgeeks/Square root of a number.cpp
@@ -0,0 +1,54 @@
+/* 
+
+Given an integer x, find the square root of x. If x is not a perfect square, then return floor(√x).
+
+ 
+
+Example 1:
+
+Input:
+x = 5
+Output: 2
+Explanation: Since, 5 is not a perfect 
+square, floor of square_root of 5 is 2.
+Example 2:
+
+Input:
+x = 4
+Output: 2
+Explanation: Since, 4 is a perfect 
+square, so its square root is 2.
+ 
+
+Your Task:
+You don't need to read input or print anything. The task is to complete the function floorSqrt() which takes x as the input parameter and return its square root.
+Note: Try Solving the question without using sqrt Function.
+
+ 
+
+Expected Time Complexity: O(log N)
+Expected Auxiliary Space: O(1)
+
+*/
+
+long long int floorSqrt(long long int x) 
+    {
+        int low = 1;
+        int high = x;
+        int ans = -1;
+        while(low <= high){
+            long long int mid = (low+high) /2;
+            long long int sqr = mid*mid;
+            if(sqr == x)
+                return mid;
+            else if(sqr < x){
+                ans = mid;
+                low = mid+1; 
+            }
+            else
+                high = mid-1;
+                
+        }
+        return ans;
+    }
+

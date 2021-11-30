9753ec250e8db00ff71a499195b838cbbe9de637
tarun <arunaggarwal096@gmail.com>
2021-11-30 19:29:16 +0530

    gfg question

--- geeksforgeeks/Peek element.cpp
+++ geeksforgeeks/Peek element.cpp
@@ -73,4 +73,24 @@ int peakElement(int arr[], int n)
 
 // method 2
 
+int peakElement(int arr[], int n)
+{
+   // Your code here
+   int low = 0;
+   int high = n-1;
+   
+   while(low<=high){
+       int mid = (low+high) / 2;
+       
+       if((mid == 0 || arr[mid] >= arr[mid-1]) && (mid == n-1 || arr[mid] >= arr[mid+1])){
+           return mid;
+       }else if(arr[mid] <= arr[mid+1]){
+           low = mid+1;
+       }else{
+           high = mid-1;
+       }
+   }
+   return -1;
+}
+
 

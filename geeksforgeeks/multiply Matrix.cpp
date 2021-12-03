844e470b55d9b0e4b1c46a98342adb49a77439d0
tarun <arunaggarwal096@gmail.com>
2021-12-03 17:16:17 +0530

    gfg question

---
+++ geeksforgeeks/multiply Matrix.cpp
@@ -0,0 +1,54 @@
+// Multiply Matrices 
+// School Accuracy: 48.41% Submissions: 14375 Points: 0
+// Given two square Matrices A[][] and B[][]. Your task is to complete the function multiply which stores the multiplied matrices in a new matrix C[][].
+ 
+
+// Example 1:
+
+// Input: 
+// N = 2
+// A[][] = {{7, 8}, {2 , 9}}
+// B[][] = {{14, 5}, {5, 18}}
+
+// Output: 
+// C[][] = {{188, 359}, {73, 172}}
+ 
+
+// Example 2:
+
+// Input: 
+// N = 2
+// A[][] = {{17, 4}, {17 , 16}}
+// B[][] = {{9, 2}, {7, 1}}
+
+// Output: 
+// C[][] = {{181, 38}, {265, 50}}
+// Constraints:
+// 1 <=T<= 100
+// 1 <= N <= 20
+
+// View Bookmarked Problems
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

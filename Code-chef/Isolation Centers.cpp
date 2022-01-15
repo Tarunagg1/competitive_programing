6017212673368ce3802b7d7d4b557f593930444a
tarun <arunaggarwal096@gmail.com>
2022-01-15 13:01:40 +0530

    codeChef may problems solutions

--- Code-chef/Isolation Centers.cpp
+++ Code-chef/Isolation Centers.cpp
@@ -48,6 +48,42 @@
 
 
 
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int n,q;
+	    cin>>n>>q;
+	    string s;
+	    cin>>s;
+	    
+	   int freq[26]={0};
+	   
+	   for(int i=0; i<s.length(); i++){
+	       freq[s[i] - 'a']++;
+	   }
+	   
+	   for(int i=0; i<q; i++){
+	       int m;
+	       cin>>m;
+	       int count = 0;
+	       
+	       for(int j=0; j<26; j++){
+	           if(freq[j] > m){
+	               count += freq[j]-m;
+	           }
+	       }
+	       cout<<count<<endl;
+	   }
+	    
+	}
+	
+	return 0;
+}
 
 
 

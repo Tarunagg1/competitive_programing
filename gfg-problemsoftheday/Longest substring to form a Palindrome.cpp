5316deb3c7d24be9610b2abae0360ba122847ac8
tarun <tarun@iotric.com>
2022-03-27 13:14:52 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Longest substring to form a Palindrome.cpp
@@ -0,0 +1,88 @@
+// Longest substring to form a Palindrome 
+// Hard Accuracy: 51.93% Submissions: 2381 Points: 8
+// Given a string S which only contains lowercase alphabets. Find the length of the longest substring of S such that the characters in it can be rearranged to form a palindrome.
+
+
+// Example 1:
+
+// Input:
+// S = "aabe"
+// Output:
+// 3
+// Explanation:
+// The substring "aab" can be rearranged to
+// "aba" which is the longest palindrome
+// possible for this String.
+// Example 2:
+// Input:
+// S = "adbabd"
+// Output:
+// 6
+// Explanation:
+// The whole string “adbabd” can be
+// rearranged to form a palindromic substring.
+// One possible arrangement is "abddba".
+// Thus, output length of the string is 6. 
+
+// Your Task:  
+// You don't need to read input or print anything. Your task is to complete the function longestSubstring() which takes a String S as input and returns the length of largest possible Palindrome.
+
+
+// Expected Time Complexity: O(|S|*26)
+// Expected Auxiliary Space: O(|S|*26)
+
+
+// Constraints:
+// 1 ≤ |S| ≤ 105
+
+// View Bookmarked Problems
+// Company Tags
+// Topic Tags
+
+
+class Solution {
+  public:
+    int longestSubstring(string s) {
+        // code here
+         int result = 0, mask=0;; 
+        map<int, int> m;         //To keep track of last index of each XOR operation
+        m[mask] = -1;
+        for(int i=0;i<s.size(); i++){ //for every char in the string
+            int t=s[i]-'a';
+            /*Turn the temp-th bit on if character occurs odd number of times
+            and turn off the temp-th bit off if the character occurs even number of times*/
+            mask^=(1<<t);
+            if (m[mask]) result = max(result,i - m[mask]);//pal is found from index[mask] to i
+            else m[mask] = i;// If x is not found then add its position in the index dict.
+            for(int j = 0; j < 26; j++){ // Check for the palindrome of odd length
+                int mask2 = mask^(1<<j);//cancel the occur of a char if it occurs odd number times
+                if (m[mask2]) result =max(result,i - m[mask2]);
+            }
+        }
+        return result;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

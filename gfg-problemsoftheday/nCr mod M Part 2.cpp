2bf54565a4005be3fdeacc1a91754268bd88f6c8
tarun aggarwal <tarun@iotric.com>
2022-05-31 09:27:26 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/nCr mod M Part 2.cpp
@@ -0,0 +1,62 @@
+// nCr mod M | Part 2 
+// Medium Accuracy: 16.24% Submissions: 1306 Points: 4
+// Given 3 integers n, r and m. You task is to calculate nCr%m.
+// Note: m is a square free number and largest prime factor of m <= 50.
+
+ 
+
+// Example 1:
+
+// Input: n = 5, r = 2, m = 98697
+// Output: 10
+// Explanation: 5C2 = 5!/(2!*3!) = 10%98697 = 10
+// Example 2:
+
+// Input: n = 3, r = 2, m = 98697
+// Output: 3
+// Explanation: 3C2 = 3!/(2!*1!) = 3%98697 = 3
+ 
+
+// Your Task:
+// You don't need to read or print anything. Your task is to complete the function nCrModM() which takes n, r and m as input parameter and returns nCr modulo m.
+ 
+
+// Expected Time Complexity: O(m + logmn) 
+// Expected Space Complexity: O(m)
+ 
+
+// Constraints:
+// 1 <= n <= r <= 109
+// 1 <= m <= 105
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

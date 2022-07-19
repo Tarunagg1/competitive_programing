95095742e6baf8c20afead95f62a8239faf8d5a3
--add <tarun@iotric.com>
2022-07-19 08:59:48 +0530

    gfg problem of the day

--- gfg-problemsoftheday/Count Smaller elements.cpp
+++ gfg-problemsoftheday/Count Smaller elements.cpp
@@ -30,7 +30,53 @@
 // 1 ≤ N ≤ 106
 // 0 ≤ Arri  ≤ 108
 
-
+class Solution{
+public:	
+	void merge(vector<pair<int,int>> &v,vector<int> &ans,int left,int mid,int right)
+{
+        int i=left,j=mid+1,k=0;
+        vector<pair<int,int>> temp(right-left+1);
+        // sorting in descending order
+        while(i<=mid && j<=right)
+        {
+            if(v[i].first>v[j].first)
+            {
+                ans[v[i].second]+=(right-j+1);
+                temp[k++]=v[i++];
+            }
+            else
+                temp[k++]=v[j++];
+        }
+        while(i<=mid)
+            temp[k++]=v[i++];
+        while(j<=right)
+            temp[k++]=v[j++];
+        k=0;
+        for(i=left;i<=right;i++)
+            v[i]=temp[k++];
+    }
+    void mergeSort(vector<pair<int,int>> &v,vector<int> &ans,int left,int right)
+    {
+        if(left<right)
+        {
+            int mid=left+(right-left)/2;
+            mergeSort(v,ans,left,mid);
+            mergeSort(v,ans,mid+1,right);
+            merge(v,ans,left,mid,right);
+        }
+    }
+	vector<int> constructLowerArray(int *arr, int n) {
+	    // code here
+	    vector<int> ans(n,0);
+	    vector<pair<int,int>> v;
+	    for(int i=0;i<n;i++)
+	    {
+	        v.push_back({arr[i],i});
+	    }
+	    mergeSort(v,ans,0,n-1);
+	    return ans;
+	}
+};
 
 
 

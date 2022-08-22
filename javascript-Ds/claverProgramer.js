ffc19556a8f0f926af56688efa0d99d474c66aaf
--add <tarun@iotric.com>
2022-08-22 09:02:50 +0530

    gfg problem of the day

---
+++ javascript-Ds/claverProgramer.js
@@ -0,0 +1,13 @@
+// let groseries = ['a','b','c','d','e','f']
+
+// console.log(groseries.slice(0,3));
+
+let items = [1,2,3,4,5];
+
+// let ans = items.reduce((prev,curr) => prev + curr);
+// console.log(ans);
+
+
+
+// let newIyems = items.filter((item) => item > 2);
+// console.log(newIyems);

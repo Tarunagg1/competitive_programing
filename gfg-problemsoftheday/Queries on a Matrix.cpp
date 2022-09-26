d901703e15f4a3d82df3dfc01b42af4e70fa50b5
tarun <arunaggarwal096@gmail.com>
2022-09-26 09:31:22 +0530

    gfg problem of the day

--- gfg-problemsoftheday/Queries on a Matrix.cpp
+++ gfg-problemsoftheday/Queries on a Matrix.cpp
@@ -1,8 +1,7 @@
 // Queries on a Matrix
 // HardAccuracy: 78.65%Submissions: 1941Points: 8
 // You are given a matrix of dimension n*n. All the cells are initially, zero. You are given Q queries, which contains 4 integers a b c d where (a,b) is the TOP LEFT cell and (c,d) is the Bottom Right cell of a submatrix. Now, all the cells of this submatrix have to be incremented by one. After all the Q queries have been performed. Your task is to find the final resulting Matrix.
-// Note : Zero-Based Indexing is used for cells of the matrix. 
- 
+// Note : Zero-Based Indexing is used for cells of the matrix.
 
 // Example 1:
 
@@ -14,20 +13,16 @@
 // {1,1,2,3},
 // {0,0,3,1},
 // {1,0,2,4}}.
-// Output: 
+// Output:
 // 2 2 1 1 1 0
-// 3 4 4 3 2 0 
+// 3 4 4 3 2 0
 // 3 4 3 3 2 0
 // 2 2 1 1 1 0
 // 1 1 1 1 0 0
 // 1 1 1 1 0 0
 // Explanation:After incrementing all the
-// sub-matrices of given queries we will 
+// sub-matrices of given queries we will
 // get the final output.
-
- 
-
- 
 
 // Example 2:
 
@@ -35,21 +30,19 @@
 // Queries = {
 // {0,0,3,3},
 // {0,0,2,2}}.
-// Output: 
-// 2 2 2 1 
-// 2 2 2 1  
-// 2 2 2 1 
-// 1 1 1 1 
+// Output:
+// 2 2 2 1
+// 2 2 2 1
+// 2 2 2 1
+// 1 1 1 1
 // Explanation:After incrementing all the
 // sub-matrices of given queries we will
 // get the final output.
 // Your Task:
 // You don't need to read or print anything. Your task is to complete the function solveQueries() which takes n and Queries and input parameter and returns a matrix after performing all the queries.
- 
 
 // Expected Time Complexity: O(n2)
 // Expected Space Complexity: O(n2)
- 
 
 // Constraints:
 // 1 <= n <= 1000
@@ -57,29 +50,36 @@
 // 0 <= b <= d < n
 // 1 <= No. of Queries <= 1000
 
+class Solution
+{
+public:
+    vector<vector<int>> solveQueries(int n, vector<vector<int>> Queries)
+    {
+        // Code here
+        vector<vector<int>> mat(n, vector<int>(n));
 
+        for (int i = 0; i < Queries.size(); i++)
+        {
 
+            int ti = Queries[i][0];
 
+            int tj = Queries[i][1];
 
+            int bi = Queries[i][2];
 
+            int bj = Queries[i][3];
 
+            for (int si = ti; si <= bi; si++)
+            {
 
+                for (int sj = tj; sj <= bj; sj++)
+                {
 
+                    mat[si][sj]++;
+                }
+            }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return mat;
+    }
+};

6a40d45a1b9bfe1761e7586bfa974f32fb012941
--add <tarun@iotric.com>
2022-11-01 09:33:37 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Median in a row-wise sorted Matrix.cpp
@@ -0,0 +1,72 @@
+// Median in a row-wise sorted Matrix
+// MediumAccuracy: 55.05%Submissions: 48086Points: 4
+// Lamp
+// This problem is part of GFG SDE Sheet. Click here to view more.
+
+// Given a row wise sorted matrix of size R*C where R and C are always odd, find the median of the matrix.
+
+// Example 1:
+
+// Input:
+// R = 3, C = 3
+// M = [[1, 3, 5],
+//      [2, 6, 9],
+//      [3, 6, 9]]
+// Output: 5
+// Explanation: Sorting matrix elements gives
+// us {1,2,3,3,5,6,6,9,9}. Hence, 5 is median.
+
+// Example 2:
+
+// Input:
+// R = 3, C = 1
+// M = [[1], [2], [3]]
+// Output: 2
+// Explanation: Sorting matrix elements gives
+// us {1,2,3}. Hence, 2 is median.
+
+// Your Task:
+// You don't need to read input or print anything. Your task is to complete the function median() which takes the integers R and C along with the 2D matrix as input parameters and returns the median of the matrix.
+
+// Expected Time Complexity: O(32 * R * log(C))
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 <= R, C <= 150
+// 1 <= matrix[i][j] <= 2000
+
+// View Bookmarked Problems
+
+class Solution
+{
+public:
+    int median(vector<vector<int>> &matrix, int R, int C)
+    {
+
+        // code here
+
+        int m = R * C;
+
+        int arr[m];
+
+        int count = 0;
+
+        for (int r = 0; r < R; r++)
+        {
+
+            for (int c = 0; c < C; c++)
+            {
+
+                arr[count] = matrix[r][c];
+
+                count++;
+            }
+        }
+
+        sort(arr, arr + m);
+
+        int p = (m) / 2;
+
+        return arr[p];
+    }
+};

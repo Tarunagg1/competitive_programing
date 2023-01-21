f49bc65d0327b0938bb3e71e17047f1f40309cd0
--add <tarun@iotric.com>
2023-01-21 09:24:31 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Minimum X (xor) A1.cpp
@@ -0,0 +1,77 @@
+// Minimum X (xor) A
+// MediumAccuracy: 52.16%Submissions: 92+Points: 4
+// Lamp
+// You can earn more Geekbits by participating in GFG Weekly Coding Contest
+
+// Given two integers A and B, the task is to find an integer X such that (X XOR A) is minimum possible and the count of set bit in X is equal to the count of set bits in B.
+
+// Example 1:
+
+// Input:
+// A = 3, B = 5
+// Output: 3
+// Explanation:
+// Binary(A) = Binary(3) = 011
+// Binary(B) = Binary(5) = 101
+// The XOR will be minimum when x = 3
+// i.e. (3 XOR 3) = 0 and the number
+// of set bits in 3 is equal
+// to the number of set bits in 5.
+// Example 2:
+
+// Input:
+// A = 7, B = 12
+// Output: 6
+// Explanation:
+// (7)2= 111
+// (12)2= 1100
+// The XOR will be minimum when x = 6
+// i.e. (6 XOR 7) = 1 and the number
+// of set bits in 6 is equal to the
+// number of set bits in 12.
+// Your task :
+// You don't need to read input or print anything. Your task is to complete the function minVal() that takes integer A and B as input and returns the value of X according to the question.
+
+// Expected Time Complexity : O(log MAX(A,B))
+// Expected Auxiliary Space : O(1)
+
+// Constraints :
+// 1 <= A, B <= 109
+
+// View Bookmarked Problems
+
+class Solution
+{
+public:
+    int minVal(int a, int b)
+    {
+        // code here
+        int cnt = 0, x = 0;
+        while (b > 0)
+        {
+
+            if (b % 2 > 0)
+                cnt++;
+            b /= 2;
+        }
+        for (int i = 31; i >= 0 && cnt > 0; i--)
+        {
+            int mask = 1 << i;
+            if (a & mask)
+            {
+                x = x | mask;
+                cnt--;
+            }
+        }
+        for (int i = 0; i <= 31 && cnt > 0; i++)
+        {
+            int mask = 1 << i;
+            if ((a & mask) == 0)
+            {
+                x = x | mask;
+                cnt--;
+            }
+        }
+        return x;
+    }
+};

852721b9e3a2fce8e081365b7f2f753046ae0e98
--add <tarun@iotric.com>
2023-05-06 10:57:33 +0530

    gfg problem of the day'

---
+++ gfg-problemsoftheday/Another Coin Change Problem.cpp
@@ -0,0 +1,77 @@
+// Another Coin Change Problem
+// MediumAccuracy: 74.19%Submissions: 5K+Points: 4
+// Finding A Job Is Now Easier Than Today's Problem! Explore Job Fair 2023
+
+// Given three integers n, k, target, and an array of coins of size n. Find if it is possible to make a change of target cents by using an infinite supply of each coin but the total number of coins used must be exactly equal to k.
+
+// Example 1:
+
+// Input:
+// n = 5, k = 3, target = 11
+// coins = {1, 10, 5, 8, 6}
+
+// Output:
+// 1
+
+// Explanation:
+// 2 coins of 5 and 1 coins of 1 can be used
+// to make change of 11 i.e. 11 => 5+5+1.
+// Example 2:
+
+// Input:
+// n = 3, k = 5, target = 25
+// coins = {7, 2, 4}
+
+// Output:
+// 1
+
+// Explanation:
+// 3 coins 7, 2 coins of 2 can be used to
+// make change of 25 i.e. 25 => 7+7+7+2+2.
+// Your Task:
+// This is a function problem. You only need to complete the function makeChanges() that 3 integers n, k, target, and an array coins as parameters and return True or False.
+
+// Expected Time Complexity: O(n*k*target)
+// Expected Space Complexity: O(k*target)
+
+// Constraints:
+// 1 <= n, k, coins[i] <= 100
+// 1 <= target <= 1000
+
+// View Bookmarked Problems
+
+class Solution
+{
+public:
+    bool caldp(int idx, int k, int tr, vector<int> &coin, vector<vector<int>> &dp)
+    {
+        if (tr == 0)
+        {
+            return (k == 0);
+            /* using an infinite supply of each coin but the total number of coins used must be exactly equal to k.*/
+        }
+        if (idx == 0)
+        {
+            return (tr / coin[idx] == k && tr % coin[idx] == 0);
+        }
+
+        if (dp[tr][k] != -1)
+        {
+            return dp[tr][k];
+        }
+        bool nttk = caldp(idx - 1, k, tr, coin, dp);
+        bool ttk = false;
+        if (tr >= coin[idx])
+        {
+            ttk = caldp(idx, k - 1, tr - coin[idx], coin, dp);
+        }
+        return dp[tr][k] = (ttk || nttk);
+    }
+
+public:
+    bool makeChanges(int n, int k, int target, vector<int> &coins)
+    {
+        vector<vector<int>> dp(target + 1, (vector<int>(k + 1, -1)));
+        return caldp(n - 1, k, target, coins, dp);
+    }
+};

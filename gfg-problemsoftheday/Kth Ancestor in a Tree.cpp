c7dbcdc79037ad8743b52c36031b0f405c79ae7c
--add <tarun@iotric.com>
2023-07-26 10:05:22 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Kth Ancestor in a Tree.cpp
@@ -0,0 +1,79 @@
+// Kth Ancestor in a Tree
+// MediumAccuracy: 35.06%Submissions: 59K+Points: 4
+// POTD July Placement Special : All POTD in the month of July are based on popular interview questions. Solve every day to ace the upcoming Placement Season !
+// Given a binary tree of size  N, a node, and a positive integer k., Your task is to complete the function kthAncestor(), the function should return the kth ancestor of the given node in the binary tree. If there does not exist any such ancestor then return -1.
+// Note: It is guaranteed that the node exists in the tree.
+
+// Example 1:
+
+// Input:
+// K = 2 Node = 4
+// Output: 1
+// Explanation:
+// Since, K is 2 and node is 4, so we
+// first need to locate the node and
+// look k times its ancestors.
+// Here in this Case node 4 has 1 as his
+// 2nd Ancestor aka the Root of the tree.
+// Example 2:
+
+// Input:
+// k=1
+// node=3
+//       1
+//     /   \
+//     2     3
+
+// Output:
+// 1
+// Explanation:
+// K=1 and node=3 ,Kth ancestor of node 3 is 1.
+// Your Task:
+// You are asked to complete the function kthAncestor() which accepts root of the tree, k and node as input parameters, and returns the kth ancestor of Node which contains node as its value.
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(N)
+
+// Constraints:
+// 1<=N<=105
+// 1<= K <= 100
+// 1 <= Node.data <= N
+
+Node *solve(Node *root, int &k, int node)
+{
+    if (root == NULL)
+        return NULL;
+    if (root->data == node)
+        return root;
+    Node *left = solve(root->left, k, node);
+    Node *right = solve(root->right, k, node);
+    if (left != NULL && right == NULL)
+    {
+        k--;
+        if (k <= 0)
+        {
+            k = INT_MAX;
+            return root;
+        }
+        return left;
+    }
+    if (right != NULL && left == NULL)
+    {
+        k--;
+        if (k <= 0)
+        {
+            k = INT_MAX;
+            return root;
+        }
+        return right;
+    }
+    return NULL;
+}
+int kthAncestor(Node *root, int k, int node)
+{
+    // Code here
+    Node *ans = solve(root, k, node);
+    if (ans == NULL || ans->data == node)
+        return -1;
+    return ans->data;
+}

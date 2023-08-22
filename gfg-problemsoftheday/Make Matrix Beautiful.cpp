44a4b6b89cfd48c14229bfc774a322b6be0b9128
--add <tarun@iotric.com>
2023-08-22 10:03:11 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Make Matrix Beautiful.cpp
@@ -0,0 +1,89 @@
+// Make Matrix Beautiful
+// MediumAccuracy: 64.75%Submissions: 19K+Points: 4
+// A beautiful matrix is a matrix in which the sum of elements in each row and column is equal. Given a square matrix of size NxN. Find the minimum number of operation(s) that are required to make the matrix beautiful. In one operation you can increment the value of any one cell by 1.
+// Example 1:
+
+// Input:
+// N = 2
+// matrix[][] = {{1, 2},
+//               {3, 4}}
+// Output:
+// 4
+// Explanation:
+// Updated matrix:
+// 4 3
+// 3 4
+// 1. Increment value of cell(0, 0) by 3
+// 2. Increment value of cell(0, 1) by 1
+// Hence total 4 operation are required.
+// Example 2:
+
+// Input:
+// N = 3
+// matrix[][] = {{1, 2, 3},
+//               {4, 2, 3},
+//               {3, 2, 1}}
+// Output:
+// 6
+// Explanation:
+// Number of operations applied on each cell are as follows:
+// 1 2 0
+// 0 0 0
+// 0 1 2
+// Such that all rows and columns have sum of 9.
+// Your Task:
+// You don't need to read input or print anything. Complete the function findMinOpeartion() that takes matrix and n as input parameters and returns the minimum number of operations.
+
+// Expected Time Complexity: O(N * N)
+// Expected Auxiliary Space: O(N)
+
+// Constraints:
+// 1 <= N <= 103
+// 1 <= matrix[i][j] <= 106
+
+class Solution
+{
+public:
+    // Function to find minimum number of operations that are required
+    // to make the matrix beautiful.
+    int findMinOpeartion(vector<vector<int>> matrix, int n)
+    {
+        // code here
+        vector<int> row(n, 0);
+        vector<int> col(n, 0);
+        int mx = 0;
+        for (int i = 0; i < n; i++)
+        {
+            long long int sum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                sum += matrix[i][j];
+            }
+            row[i] = sum;
+            mx = max(mx, row[i]);
+        }
+        for (int j = 0; j < n; j++)
+        {
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += matrix[i][j];
+            }
+            col[j] = sum;
+            mx = max(mx, col[j]);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int a1 = max(row[i], col[j]);
+                int r = mx - a1;
+                ans += r;
+                row[i] += r;
+                col[j] += r;
+            }
+        }
+        return ans;
+    }
+};

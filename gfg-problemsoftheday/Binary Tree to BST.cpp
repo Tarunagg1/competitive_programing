4a133f4c49dfac5851bcfde045ee0f7cc197dea3
--add <tarun@iotric.com>
2023-09-08 09:43:05 +0530

    gfg problem of the day

--- gfg-problemsoftheday/Binary Tree to BST.cpp
+++ gfg-problemsoftheday/Binary Tree to BST.cpp
@@ -7,10 +7,10 @@
 //       1
 //     /   \
 //    2     3
-// Output: 
+// Output:
 // 1 2 3
 // Explanation:
-// The converted BST will be 
+// The converted BST will be
 //       2
 //     /   \
 //    1     3
@@ -21,9 +21,9 @@
 //           1
 //        /    \
 //      2       3
-//    /        
-//  4       
-// Output: 
+//    /
+//  4
+// Output:
 // 1 2 3 4
 // Explanation:
 // The converted BST will be
@@ -42,69 +42,47 @@
 // Constraints:
 // 1 <= Number of nodes <= 105
 
-
-
-class Solution{
-  public:
+class Solution
+{
+public:
     // The given root is the root of the Binary Tree
     // Return the root of the generated BST
-    void inorder(Node* root,vector<int>&nodelist){
-        //base case
-        if(root==NULL){
+    void inorder(Node *root, vector<int> &nodelist)
+    {
+        // base case
+        if (root == NULL)
+        {
             return;
         }
-        //LNR(inorder)
-        inorder(root->left,nodelist);
+        // LNR(inorder)
+        inorder(root->left, nodelist);
         nodelist.push_back(root->data);
-        inorder(root->right,nodelist);
+        inorder(root->right, nodelist);
     }
-    Node* sortedArrTOBST(int start,int end,vector<int>&nodelist){ 
-        //base case
-        if(start>end){
+    Node *sortedArrTOBST(int start, int end, vector<int> &nodelist)
+    {
+        // base case
+        if (start > end)
+        {
             return NULL;
         }
-        
-        int mid = start+(end-start)/2;
-        Node* newNode = new Node(nodelist[mid]);
-        //recursive call root->left and root->right
-        newNode->left = sortedArrTOBST(start,mid-1,nodelist);
-        newNode->right = sortedArrTOBST(mid+1,end,nodelist);
-        
+
+        int mid = start + (end - start) / 2;
+        Node *newNode = new Node(nodelist[mid]);
+        // recursive call root->left and root->right
+        newNode->left = sortedArrTOBST(start, mid - 1, nodelist);
+        newNode->right = sortedArrTOBST(mid + 1, end, nodelist);
+
         return newNode;
     }
-    Node *binaryTreeToBST (Node *root)
+    Node *binaryTreeToBST(Node *root)
     {
-        vector<int>nodelist;
-        //extract values using inorder traversal
+        vector<int> nodelist;
+        // extract values using inorder traversal
         inorder(root, nodelist);
-        
+
         sort(nodelist.begin(), nodelist.end());
-        //convert the sorted values back to BST
-        return sortedArrTOBST(0,nodelist.size()-1,nodelist);
+        // convert the sorted values back to BST
+        return sortedArrTOBST(0, nodelist.size() - 1, nodelist);
     }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

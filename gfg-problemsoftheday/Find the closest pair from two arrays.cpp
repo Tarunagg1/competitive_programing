f33c37b6a5430a4da3091e7a8ac7c70ad7902985
--add <tarun@iotric.com>
2023-09-27 10:16:53 +0530

    gfg problem of the day

--- gfg-problemsoftheday/Find the closest pair from two arrays.cpp
+++ gfg-problemsoftheday/Find the closest pair from two arrays.cpp
@@ -1,23 +1,27 @@
 
 
-class Solution{
-  public:
-    vector<int> printClosest(int arr[], int brr[], int n, int m, int x) {
-        //code here
-         int i=0,j=m-1,MIN_DIFF = INT_MAX;
+class Solution
+{
+public:
+    vector<int> printClosest(int arr[], int brr[], int n, int m, int x)
+    {
+        // code here
+        int i = 0, j = m - 1, MIN_DIFF = INT_MAX;
         vector<int> INDEXES(2);
-        while(i<n and j>=0)
+        while (i < n and j >= 0)
         {
             int sum = arr[i] + brr[j];
-            int DIFF = abs(sum-x);
-            if(MIN_DIFF > DIFF)
+            int DIFF = abs(sum - x);
+            if (MIN_DIFF > DIFF)
             {
                 INDEXES[0] = arr[i];
                 INDEXES[1] = brr[j];
                 MIN_DIFF = DIFF;
             }
-            if(sum < x) i++;
-            else j--;
+            if (sum < x)
+                i++;
+            else
+                j--;
         }
         return INDEXES;
     }

ec8972739d1059afbb83d6849ef94dee391ca194
--add <tarun@iotric.com>
2023-11-02 10:37:13 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Minimum distance between two numbers.cpp
@@ -0,0 +1,67 @@
+// Minimum distance between two numbers
+// EasyAccuracy: 19.75%Submissions: 176K+Points: 2
+// GfG Weekly + You = Perfect Sunday Evenings!
+// Register for free now
+
+// banner
+// You are given an array a, of n elements. Find the minimum index based distance between two distinct elements of the array, x and y. Return -1, if either x or y does not exist in the array.
+
+// Example 1:
+
+// Input:
+// N = 4
+// A[] = {1,2,3,2}
+// x = 1, y = 2
+// Output: 1
+// Explanation: x = 1 and y = 2. There are
+// two distances between x and y, which are
+// 1 and 3 out of which the least is 1.
+// Example 2:
+
+// Input:
+// N = 7
+// A[] = {86,39,90,67,84,66,62}
+// x = 42, y = 12
+// Output: -1
+// Explanation: x = 42 and y = 12. We return
+// -1 as x and y don't exist in the array.
+// Your Task:
+// Complete the function minDist() which takes the array a, and 3 integers n, x and y as input parameters and returns the minimum distance between x and y in the array. Return -1, if either x or y does not exist in the array.
+
+// Expected Time Complexity: O(N)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 <= n <= 105
+// 0 <= a[i], x, y <= 105
+// x != y
+
+class Solution
+{
+public:
+    int minDist(int a[], int n, int x, int y)
+    {
+        // code here
+
+        int pos = -1;
+        bool found = false;
+        int ans = n;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (a[i] == x || a[i] == y)
+            {
+                if (pos > -1 && a[pos] != a[i])
+                {
+                    ans = min(ans, abs(pos - i));
+                    found = true;
+                }
+                pos = i;
+            }
+        }
+
+        if (!found)
+            return -1;
+        return ans;
+    }
+};

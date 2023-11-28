e5b4414222d889ad1ecca3b9b967931ad7192868
--add <tarun@iotric.com>
2023-11-28 10:52:59 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Detect Cycle using DSU.cpp
@@ -0,0 +1,53 @@
+// Detect Cycle using DSU
+// MediumAccuracy: 48.37%Submissions: 21K+Points: 4
+// GfG Weekly + You = Perfect Sunday Evenings!
+// Register for free now
+
+// banner
+// Given an undirected graph with no self loops with V (from 0 to V-1) nodes and E edges, the task is to check if there is any cycle in the undirected graph.
+
+// Note: Solve the problem using disjoint set union (DSU).
+
+// Example 1:
+
+// Input: 
+
+// Output: 1
+// Explanation: There is a cycle between 0->2->4->0
+// Example 2:
+
+// Input: 
+
+// Output: 0
+// Explanation: The graph doesn't contain any cycle
+// Your Task:
+// You don't need to read or print anyhting. Your task is to complete the function detectCycle() which takes number of vertices in the graph denoting as V and adjacency list adj and returns 1 if graph contains any cycle otherwise returns 0.
+
+// Expected Time Complexity: O(V + E)
+// Expected Space Complexity: O(V)
+
+// Constraints:
+// 2 ≤ V ≤ 104
+// 1 ≤ E ≤ 104
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

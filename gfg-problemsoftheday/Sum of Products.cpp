a46fa6c6b2345791f76ccaad199aa20b246b98a0
--add <tarun@iotric.com>
2024-04-12 11:59:06 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Sum of Products.cpp
@@ -0,0 +1,58 @@
+// Sum of Products
+// MediumAccuracy: 50.93%Submissions: 11K+Points: 4
+// Given an array arr[] of size n. Calculate the sum of Bitwise ANDs ie: calculate sum of arr[i] & arr[j] for all the pairs in the given array arr[] where i < j.
+
+// Example 1:
+
+// Input:
+// n = 3
+// arr = {5, 10, 15}
+// Output:
+// 15
+// Explanation:
+// The bitwise Ands of all pairs where i<j are (5&10) = 0, (5&15) = 5 and (10&15) = 10.
+// Therefore, the total sum = (0+5+10) = 15.
+// Example 2:
+
+// Input:
+// n = 4
+// arr = {10, 20, 30, 40}
+// Output:
+// 46
+// Explanation:
+// The sum of bitwise Ands
+// of all pairs = (0+10+8+20+0+8) = 46.
+// Your Task:
+// You don't need to read input or print anything.Your Task is to complete the function pairAndSum() which takes an Integer n and an array arr[]  of size n as input parameters and returns the sum of bitwise Ands of all pairs.
+
+// Expected Time Complexity:O(n)
+// Expected Auxillary Space:O(1)
+
+// Constraints:
+// 1 <= n <= 105
+// 1 <= arr[i] <= 108
+
+class Solution
+{
+public:
+    long long pairAndSum(int n, long long arr[])
+    {
+        // code here
+        long long ans = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            long long int c = 0;
+            long long int t = 1 << i;
+            for (int j = 0; j < n; j++)
+            {
+
+                if (t & arr[j])
+                {
+                    c++; // no.of elemenets that has 1's on same bit
+                }
+            }
+            ans += (c * (c - 1) / 2) * (t); // nc2 and converting into decimal also
+        }
+        return (ans);
+    }
+};

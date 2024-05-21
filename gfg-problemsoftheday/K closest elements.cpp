b566cef5a4d51b2e17bffca05c5cccf60bf3a13d
--add <tarun@iotric.com>
2024-05-21 12:38:48 +0530

    gfg problem of the day

--- gfg-problemsoftheday/K closest elements.cpp
+++ gfg-problemsoftheday/K closest elements.cpp
@@ -9,16 +9,16 @@
 // If x is present in the array, then it need not be considered.
 // If two elements have the same difference as x, the greater element is prioritized.
 // If sufficient elements are not present on the right side, take elements from the left and vice versa.
-
+ 
 // Example 1:
 
 // Input:
 // n = 13
-// arr[] = {12, 16, 22, 30, 35, 39, 42,
+// arr[] = {12, 16, 22, 30, 35, 39, 42, 
 //          45, 48, 50, 53, 55, 56}
 // k = 4, x = 35
 // Output: 39 30 42 45
-// Explanation:
+// Explanation: 
 // First closest element to 35 is 39.
 // Second closest element to 35 is 30.
 // Third closest element to 35 is 42.
@@ -31,18 +31,20 @@
 // arr[] = {1, 2, 3, 6, 10}
 // k = 3, x = 4
 // Output: 3 6 2
-// Explanation:
+// Explanation: 
 // First closest element is 3.
-// There are two elements 2 and 6 for which
+// There are two elements 2 and 6 for which 
 // the difference with 4 is same i.e. 2.
-// So first take greatest number 6
+// So first take greatest number 6 
 // then the lower number 2.
 
 // Your Task:
 // You don't need to read input or print anything. Complete the function printKClosest() which takes arr[], n, k, and x as input parameters and returns an array of integers containing the K closest elements to x in arr[].
 
+
 // Expected Time Complexity: O(log n + k)
 // Expected Auxiliary Space: O(k)
+
 
 // Constraints:
 // 1 ≤ n ≤ 105
@@ -50,30 +52,46 @@
 // 1 ≤ x ≤ 106
 // 1 ≤ arr[i] ≤ 106
 
-class Solution
-{
-public:
-    vector<int> printKClosest(vector<int> arr, int n, int k, int x)
-    {
+
+
+class Solution {
+  public:
+    vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
         // code here
-        map<int, vector<int>> u;
-        sort(a.rbegin(), a.rend());
-        for (int i = 0; i < n; i++)
-        {
-            if (x != a[i])
-                u[abs(x - a[i])].push_back(a[i]);
+      int r = upper_bound(a.begin(), a.end(),x) - a.begin();
+        int l = lower_bound(a.begin(), a.end(), x) - a.begin() - 1;
+        vector<int>ans(k);
+        int ptr = 0;
+        while((l >= 0 || r < n) && ptr < k){
+            int leftdiff = INT_MAX, rightdiff = INT_MAX;
+            if(l >= 0)
+                leftdiff = x - a[l];
+            if(r < n)
+                rightdiff = a[r] - x;
+            
+            if(rightdiff <= leftdiff)
+                ans[ptr++] = a[r++];
+            else
+                ans[ptr++] = a[l--];
+            
         }
-        vector<int> p;
-        for (auto e : u)
-        {
-            for (auto f : e.second)
-            {
-                p.push_back(f);
-                k--;
-                if (k == 0)
-                    return p;
-            }
-        }
-        return p;
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

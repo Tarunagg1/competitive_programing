278dab540fd449d7d778b7ef9a24b60c782b3458
--add <tarun@iotric.com>
2024-09-28 13:19:00 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Minimal Cost.cpp
@@ -0,0 +1,43 @@
+// Minimal Cost
+// Difficulty: MediumAccuracy: 51.08%Submissions: 55K+Points: 4
+// There is an array arr of heights of stone and Geek is standing at the first stone and can jump to one of the following: Stone i+1, i+2, ... i+k stone, where k is the maximum number of steps that can be jumped and cost will be |hi-hj| is incurred, where j is the stone to land on. Find the minimum possible total cost incurred before the Geek reaches the last stone.
+
+// Example:
+
+// Input: k = 3, arr[]= [10, 30, 40, 50, 20]
+// Output: 30
+// Explanation: Geek will follow the path 1->2->5, the total cost would be | 10-30| + |30-20| = 30, which is minimum
+// Input: k = 1, arr[]= [10, 20, 10]
+// Output: 20
+// Explanation: Geek will follow the path 1->2->3, the total cost would be |10 - 20| + |20 - 10| = 20.
+// Expected Time Complexity: O(n*k)
+// Expected Auxilary Space: O(n)
+
+// Constraint:
+// 1<= arr.size() <=104
+// 1 <= k <= 100
+// 1 <= arr[i] <= 104
+
+// Seen this question in a real interview before ?
+
+class Solution
+{
+public:
+    int minimizeCost(int k, vector<int> &arr)
+    {
+        // Code here
+        int n = arr.size();
+        vector<int> dp(n, INT_MAX);
+        dp[n - 1] = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int j = 1; j <= k; j++)
+            {
+                if (i + j >= n)
+                    break;
+                dp[i] = min(dp[i], abs(arr[j + i] - arr[i]) + dp[i + j]);
+            }
+        }
+        return dp[0];
+    }
+};

56dbb21a83b339d6db79b03c24ebae201f64adbc
--add <tarun@iotric.com>
2024-10-22 12:03:33 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Sub-arrays with equal number of occurences.cpp
@@ -0,0 +1,43 @@
+// Sub-arrays with equal number of occurences
+// Difficulty: HardAccuracy: 69.36%Submissions: 6K+Points: 8
+// Given an array arr[] and two integers say, x and y, find the number of sub-arrays in which the number of occurrences of x is equal to the number of occurrences of y.
+
+// Examples:
+
+// Input: arr[] = [1, 2, 1] , x= 1 , y = 2
+// Output: 2
+// Explanation: The possible sub-arrays have same equal number of occurrences of x and y are:
+// 1) [1, 2], x and y have same occurrence(1).
+// 2) [2, 1], x and y have same occurrence(1).
+// Input: arr[] = [1, 2, 1] , x = 4 , y = 6
+// Output: 6
+// Explanation: The possible sub-arrays have same equal number of occurrences of x and y are:
+// 1) [1], x and y have same occurrence(0).
+// 2) [2], x and y have same occurrence(0).
+// 3) [1], x and y have same occurrence(0).
+// 4) [1, 2], x and y have same occurrence(0).
+// 5) [2, 1], x and y have same occurrence(0).
+// 6) [1, 2, 1], x and y have same occurrence(0).
+// Input: arr[] = [1, 2, 1] , x= 1 , y = 4
+// Output: 1
+// Explanation: The possible sub-array have same equal number of occurrences of x and y is: [2], x and y have same occurrence(0)
+// Constraints:
+// 1 <= arr.size() <= 106
+// 1 <= arr[i], x, y<=106
+
+class Solution
+{
+public:
+    int sameOccurrence(vector<int> &arr, int x, int y)
+    {
+        // code here
+        unordered_map<int, int> mp;
+        int ans = 0, sum = 0, i = 0;
+        for (i = 0, ++mp[0]; i < arr.size(); i++, ans += mp[sum]++)
+        {
+            sum += arr[i] == x;
+            sum -= arr[i] == y;
+        }
+        return ans;
+    }
+};

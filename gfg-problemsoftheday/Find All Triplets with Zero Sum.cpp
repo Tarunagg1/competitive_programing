9647459fe616c6c512136df135e5f492c562c3ba
--add <tarun@iotric.com>
2024-11-04 11:12:41 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Find All Triplets with Zero Sum.cpp
@@ -0,0 +1,48 @@
+// Find All Triplets with Zero Sum
+// Difficulty: EasyAccuracy: 64.89%Submissions: 6K+Points: 2
+// Given an array arr[], find all possible indices [i, j, k] of triplets [arr[i], arr[j], arr[k]] in the array whose sum is equal to zero. Return indices of triplets in any order and all the returned triplets indices should also be internally sorted, i.e., for any triplet indices [i, j, k], the condition i < j < k should hold.
+
+// Examples:
+
+// Input: arr[] = [0, -1, 2, -3, 1]
+// Output: [[0, 1, 4], [2, 3, 4]]
+// Explanation: Triplets with sum 0 are:
+// arr[0] + arr[1] + arr[4] = 0 + (-1) + 1 = 0
+// arr[2] + arr[3] + arr[4] = 2 + (-3) + 1 = 0
+// Input: arr[] = [1, -2, 1, 0, 5]
+// Output: [[0, 1, 2]]
+// Explanation: Only triplet which satisfies the condition is arr[0] + arr[1] + arr[2] = 1 + (-2) + 1 = 0
+// Input: arr[] = [2, 3, 1, 0, 5]
+// Output: [[]]
+// Explanation: There is no triplet with sum 0.
+// Constraints:
+// 3 <= arr.size() <= 103
+// -104 <= arr[i] <= 104
+
+class Solution
+{
+public:
+    vector<vector<int>> findTriplets(vector<int> &arr)
+    {
+        // Code here
+        vector<vector<int>> ans;
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            int a = arr[i];
+            for (int j = i + 1; j < n; j++)
+            {
+                int b = arr[j];
+                for (int k = j + 1; k < n; k++)
+                {
+                    int c = arr[k];
+                    if (a + b + c == 0)
+                    {
+                        ans.push_back({i, j, k});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};

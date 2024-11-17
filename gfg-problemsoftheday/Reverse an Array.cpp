a29b2a0aad3dc3de220edf2f0b7eeab6fafd4db1
--add <tarun@iotric.com>
2024-11-17 12:29:00 +0530

    gfg problem of the day

---
+++ gfg-problemsoftheday/Reverse an Array.cpp
@@ -0,0 +1,32 @@
+// Reverse an Array
+// Difficulty: EasyAccuracy: 55.32%Submissions: 45K+Points: 2
+// You are given an array of integers arr[]. Your task is to reverse the given array.
+
+// Examples:
+
+// Input: arr = [1, 4, 3, 2, 6, 5]
+// Output: [5, 6, 2, 3, 4, 1]
+// Explanation: The elements of the array are 1 4 3 2 6 5. After reversing the array, the first element goes to the last position, the second element goes to the second last position and so on. Hence, the answer is 5 6 2 3 4 1.
+// Input: arr = [4, 5, 2]
+// Output: [2, 5, 4]
+// Explanation: The elements of the array are 4 5 2. The reversed array will be 2 5 4.
+// Input: arr = [1]
+// Output: [1]
+// Explanation: The array has only single element, hence the reversed array is same as the original.
+// Constraints:
+// 1<=arr.size()<=105
+// 0<=arr[i]<=105
+
+class Solution
+{
+public:
+    void reverseArray(vector<int> &arr)
+    {
+        // code here
+        int n = arr.size();
+        for (int i = 0; i < n / 2; i++)
+        {
+            swap(arr[i], arr[n - 1 - i]);
+        }
+    }
+};
